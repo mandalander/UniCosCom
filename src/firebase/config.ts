e9dac25d51c9ebcf68f83a8bb667183d5c313382
Dawid Konstańczuk <admin@unicoscom.com>
2025-11-11 12:29:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1081495655-97074",
+  "appId": "1:1066283365530:web:13b64d678d10120e759b23",
+  "apiKey": "AIzaSyCpGS6y5NAPFOuwz4Aos1c0mRdB4yAWMeg",
+  "authDomain": "studio-1081495655-97074.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1066283365530"
+};
