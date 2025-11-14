f67b81c8fc05f936a36df5668d1ca8b3687b1c34
Dawid Konstańczuk <admin@unicoscom.com>
2025-11-14 00:27:06 +0000

    adsense prosi mnie o to

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7558071569357753" crossOrigin="anonymous"></script>
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
