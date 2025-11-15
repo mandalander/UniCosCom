af43372d0fc2995a0070e01ba902c6faadabc0d6
Dawid Konstańczuk <admin@unicoscom.com>
2025-11-15 00:14:52 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
 export const firebaseConfig = {
-  "projectId": "studio-1081495655-97074",
-  "appId": "1:1066283365530:web:2f2f0e64c99a5ef7759b23",
   "apiKey": "AIzaSyCpGS6y5NAPFOuwz4Aos1c0mRdB4yAWMeg",
   "authDomain": "studio-1081495655-97074.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1066283365530"
+  "databaseURL": "https://studio-1081495655-97074-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "studio-1081495655-97074",
+  "storageBucket": "studio-1081495655-97074.firebasestorage.app",
+  "messagingSenderId": "1066283365530",
+  "appId": "1:1066283365530:web:b42c71db0e5f37cc759b23",
+  "measurementId": "G-4MZM0THMX2"
 };
